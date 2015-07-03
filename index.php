add91d28dc620bbc191cf7d4f1a48a18383e93db
borisunknown <bogdan.lozyak.97@outlook.com>
2015-07-03 17:22:54 +0200

    Aggiornamento tutti i file
    -Stampa dati da php in base al tipo di certificato
    -Ulteeriori passi nella grafica della versone mobile

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
-<?php
+<?php 
+/*analisi richiesta cerificato*/
+
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+	
+	$idCertificato = $_GET['ID_Certificato'];
+	
+	if(!empty($idCertificato) && null != $idCertificato) {
+		$idCertificato = test_input($idCertificato);
+		
+	//Controllo se la stringa è lunga 30 caratteri	
+	include "php/Certificate_class.php";
+	$_GLOBALS['certificato'] = new Document();
+	
+}
+
+
+// Funzioni
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+ }
 
 ?>
 
@@ -11,18 +34,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title>Certification Service</title>
-<link type="text/css" href="css/bootstrap.css">
+<link type="text/css" href="css/bootstrap.css" media="all">
 
-<script type="text/javascript">
-
-</script>
+<script type="text/javascript"></script>
 
 </head>
 
 <body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>s
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jQuery_server_request.js"></script>
+<script src="js/jQuery_design.js"></script>
 
 <header>
 <button id="btnAllCertiicate" title="Tutti i certificati" ></button>
@@ -57,22 +79,28 @@
         <div class="col-md-4">
             <section id="sectionInfoCertificato">
             
-            <div id="utente_titolo"></div>
+            <div id="utente_titolo"><?php echo $_GLOBALS['certificato']->titolo; ?></div>
             <div id="identitaUtente">
-                <div id="utente_nome"></div>
-                <div id="utente_cognome"></div>
+                <div id="utente_nome"><?php echo $_GLOBALS['certificato']->nome; ?></div>
+                <div id="utente_cognome"><?php echo $_GLOBALS['certificato']->cognome; ?></div>
             </div>
             
-            <div id="utente_luogoRilascio"></div>
-            <div id="utente_dataRilascio"></div>
-            <div id="utente_durataCorso"></div>
-            <div id="utente_identitaDocente"></div>
-            <div id="utente_descrizione"></div>
+            <div id="utente_luogoRilascio"><?php echo $_GLOBALS['certificato']->luogoRilascio; ?></div>
+            <div id="utente_dataRilascio"><?php echo $_GLOBALS['certificato']->dataRilascio; ?></div>
+            
+			<?php 
+			/*Stampa se è un certificato di un corso*/
+			if($_GLOBALS['certificato']->isCertificato) {
+				echo '<div id="utente_durataCorso">', $_GLOBALS['certificato']->durataCorso, '</div>
+				<div id="utente_identitaDocente">', $_GLOBALS['certificato']->identitaUtente, '</div>';
+            } ?>
+            <div id="utente_descrizione"><?php echo $_GLOBALS['certificato']->descrizione; ?></div>
+            
             </section>
         </div>
         <div class="col-md-4">
             <section id="sectionFotoCertificato" >
-                <img id="imgCertificato" title="<?php echo certificate.title; ?>" src="<?php echo $certificate.photoLocation; ?>"/>
+                <img alt=" " id="imgCertificato" title="<?php echo $_GLOBALS['certificato']->titolo; ?>" src="<?php echo $_GLOBALS['certificato']->imgLocation; ?>" />
             </section>
         </div>
     </div>
