640845d4b33fe4ea7b6b89fa9a4395735fed55d6
borisunknown <bogdan.lozyak.97@outlook.com>
2015-07-05 19:46:59 +0200

    Aggiornamento versione mobile e il lato server
    Mobile: aggiunta l'icona delle informazioni
    Lato Server: aggiunti i controlli per le richieste dal client

--- php/request_support.php
+++ php/request_support.php
@@ -1,5 +1,4 @@
 <?php
-include "Certificate_class";
 /**
  * Created by PhpStorm.
  * User: Bogdan
@@ -25,24 +24,64 @@ include "Certificate_class";
  *
  *
  */
+ 
+ /* CONTROLLO INPUT */
 if($_SERVER['REQUEST_METHOD'] == "POST") {
-
-
-
-
-    $idCertificato = $_POST['ID_Certificato'];
-
-    if(!empty($idCertificato) && null != $idCertificato) {
-        $idCertificato = test_input($idCertificato);
-
-        //Controllo se la stringa è lunga 30 caratteri
-        include "php/Certificate_class.php";
-        $_GLOBALS['certificato'] = new Document();
-    }
-
+	$tipoRichiesta = $_POST['typeRequest'];
+	if(!empty($tipoRichiesta))
+	{
+		$tipoRichiesta = test_input($tipoRichiesta);
+		
+		if($tipoRichiesta === "document")
+		{
+			$idCertificato = $_POST['ID_Certificato'];
+		
+			if(!empty($idCertificato)) {
+				$idCertificato = test_input($idCertificato);
+		
+				//Controllo se la stringa è lunga 30 caratteri
+				if(!true)
+				{
+					$infoCertificato = ricercaDB_certificato($idCertificato);
+					//Costruisco la risposta in HTML 
+					$rispostaPronta = risposta_infoCertificato($infoCertificato);
+					// Invio la risposta
+					echo $rispostaPronta;					
+				}
+			}
+		}
+		else if($tipoRichiesta === "allDocument")
+		{
+			$idUtente = $_POST['ID_User'];
+		
+			if(!empty($idUtente)) {
+				$idUtente = test_input($idUtente);
+		
+				//Controllo se id è lungo 10 caratteri
+				if(!true)
+				{
+					$elencoCertificati = ricercaDB_Utente($idUtente);
+					//Costruisco la risposta in HTML 
+					$rispostaPronta = risposta_elencoCertificati($elencoCertificati);
+					// Invio la risposta
+					echo $rispostaPronta;					
+				}
+			}
+		}
+	}
 }
 
+/* Funzioni di ricerca nel database */
+function ricercaDB_certificato($idRicerca)
+{
+	$requestToSQL = "SELECT * FROM Uenti WHERE idCertificato=" + $idRicerca;
+	return ;
+}
 
+function ricercaDB_Utente($idUtente)
+{
+	
+}
 /* Funzione di test richiesta da codice maligno per prevenire 
  * codice javascript eseguibile
  */
