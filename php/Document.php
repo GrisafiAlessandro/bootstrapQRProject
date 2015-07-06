dda4531c0403c37482221113bc68c4d229da0929
borisunknown <bogdan.lozyak.97@outlook.com>
2015-07-06 21:58:17 +0200

    aggiornamento

--- php/Document.php
+++ php/Document.php
@@ -6,26 +6,26 @@
  * Time: 16:50
  */
 
-class Document
-{
+class Document {
+    public $idDocumento = null;
     public $titolo = null;
-    public $nome= null;
+    public $nome = null;
     public $cognome = null;
     public $luogoRilascio = null;
     public $dataRilascio = null;
 
     //Campi certificato corso
     public $isCertificato = false;
+    //Chiave id dell'insegnante
+
+    public $idCorso = null;
     public $durataCorso = null;
     public $descrizione = null;
-    //Chiave id l'insegnante
-    public $identitaDocente = null;
-
+    public $isAttestato = false;
+    public $idDocente = null;
+    public $risultatoAttestato = null;
+    // PATH all'immagine;
 
     //campi Attestato
-    public $isAttestato = false;
-    public $risultatoAttestato = null;
-
-    // PATH all'immagine
     public $imgLocation = null;
 }
--- php/User.php
+++ php/User.php
@@ -7,5 +7,7 @@
  */
 
 class User {
-
+    public $idUtente = null;
+    public $nome = null;
+    public $cognome = null;
 }
